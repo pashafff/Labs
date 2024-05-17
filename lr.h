9a705fe7828798a5bc0371ec0ef4a4c237583326
pashafff <47026806+pashafff@users.noreply.github.com>
2024-05-18 00:08:09 +0300

    Adding files
    Adding main files

---
+++ lr.h
@@ -0,0 +1,25 @@
+#ifndef lr_H
+#define lr_H
+
+
+void first();
+void first_add();
+
+void second();
+void second_add();
+
+void third();
+void third_add();
+
+void four();
+void four_add();
+
+void five();
+void five_add();
+
+void six();
+void six_add();
+
+void seven();
+
+#endif
---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "lr.h"
+
+int main(void)
+{
+    int choice = 0;
+    while (choice < 1 || choice>13)
+    {
+        printf("1) The first laboratory work\n");
+        printf("2) The first laboratory work (add)\n");
+        printf("3) The second laboratory work\n");
+        printf("4) The second laboratory work (add)\n");
+        printf("5) The third laboratory work\n");
+        printf("6) The third laboratory work (add)\n");
+        printf("7) The four laboratory work \n");
+        printf("8) The four laboratory work (add)\n");
+        printf("9) The five laboratory work \n");
+        printf("10) The five laboratory work (add)\n");
+        printf("11) The six laboratory work \n");
+        printf("12) The six laboratory work (add)\n");
+        printf("13) The seven laboratory work \n");
+        printf("Enter the number of the desired laboratory work: ");
+        scanf("%d", &choice);
+    }
+
+    if (choice == 1)
+        first();
+    else if (choice == 2)
+        first_add();
+    else if (choice == 3)
+        second();
+    else if (choice == 4)
+        second_add();
+    else if(choice == 5)
+        third();
+    else if(choice == 6)
+        third_add();
+    else if(choice == 7)
+        four();
+    else if(choice == 8)
+        four_add();
+    else if(choice == 9)
+        five();
+    else if(choice == 10)
+        five_add();
+    else if(choice == 11)
+        six();
+    else if(choice == 12)
+        six_add();
+    else if(choice == 13)
+        seven();
+}
